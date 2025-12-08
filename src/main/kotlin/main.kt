77271ce869bd4c7195fd6d229ae63048f1110a78
Fabio Leandro Lapuinka <lapuinkafabio@gmail.com>
2025-12-07 23:39:23 -0300

    *.*

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,16 @@
+//Fabio Leandro Lapuinka fabio
+
+/**
+ * Documentação da minha função
+ * */
+fun main(){
+    var teste = "Ola, Mundo!";
+
+    println(teste)
+    println("${Double.MIN_VALUE} - ${Double.MAX_VALUE}")
+    println("${Float.MIN_VALUE} - ${Float.MAX_VALUE}")
+    println("${Long.MIN_VALUE} - ${Long.MAX_VALUE}")
+    println("${Integer.MIN_VALUE} - ${Integer.MAX_VALUE}")
+    println("${Short.MIN_VALUE} - ${Short.MAX_VALUE}")
+    println("${Byte.MIN_VALUE} - ${Byte.MAX_VALUE}")
+}
