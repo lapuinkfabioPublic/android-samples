faf9c9b3b6337862f9efd4d3fcaffdda9b405faa
Fabio Leandro Lapuinka <lapuinkafabio@gmail.com>
2025-12-13 02:29:37 -0300

    day

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,21 +1,81 @@
+import java.util.function.ToIntFunction
+
 //Fabio Leandro Lapuinka fabio
 
 /**
- * Documentação da minha função
+ * Documentação da minha função Quebrante
  * */
-fun main(){
-    val teste = "Ola, Mundo!"
+fun main(args: Array<String>) {
 
-    val ling = "Kotlin"
-    val carac = "eh show"
+    var num1: Int = 0
+    var num2: Int = 0
+    var op = "+"
+    while (op != "s") {
+        println("Digite num1:")
+        num1 = readln().toInt()
+        println("Digite num2")
+        num2 = readln().toInt()
+        println("Digite a operacao (+,/,%,++,--,(s)sair)")
+        op = readln()
 
-    println("$ling $carac")
+        when (op){
+            "+"->
+            {
+                println("Soma: ${num1 + num2}")
+            }
+            "/"->
+            {
+                println("Divisão: ${num1 / num2}")
+            }
+            "%"->
+            {
+                println("Módulo: ${num1 % num2}")
+            }
+            "++"->
+            {
+                println("Incremento num1: ${++num1}")
+                println("Incremento num2: ${++num2}")
+            }
+            "--"->
+            {
+                println("Decremento num1: ${--num1}")
+                println("Decremento num2: ${--num2}")
+            }
+            else -> println("Invalid option")
+            ;
+        }
+    }
+    println("Fim do Programa")
+    readln()
 
-    println(teste)
-    println("${Double.MIN_VALUE} - ${Double.MAX_VALUE}")
-    println("${Float.MIN_VALUE} - ${Float.MAX_VALUE}")
-    println("${Long.MIN_VALUE} - ${Long.MAX_VALUE}")
-    println("${Integer.MIN_VALUE} - ${Integer.MAX_VALUE}")
-    println("${Short.MIN_VALUE} - ${Short.MAX_VALUE}")
-    println("${Byte.MIN_VALUE} - ${Byte.MAX_VALUE}")
+}
+fun main3(args: Array<String>) {
+
+    var nome = ""
+    var numero =""
+
+    println("Digite seu nome: ")
+    nome =readln()
+
+    println("Digite seu numero: ")
+    numero = readln()
+
+    var populacao = 212500000 //populacao do Brasil em 2025
+
+    println("Seu nome é $nome")
+    println("Seu numero é $numero")
+    println("Populacao é $populacao")
+    readln()
+
+}
+fun main2(){
+
+    var base = 10
+    var altura = 2
+
+
+    println("Base: ${base} - Altura:${altura}: ${base*altura}")
+
+
+
 }
