01afb0c17babf3be672ed7292c00867fe78c3b35
Fabio Leandro Lapuinka <lapuinkafabio@gmail.com>
2025-12-25 21:02:39 -0300

    day

--- src/main/kotlin/LeituraDados.kt
+++ src/main/kotlin/LeituraDados.kt
@@ -1,4 +1,8 @@
 fun main(){
+
+    println("Dados: ")
+    println(chars("Hello World!"))
+
     println("Digite o valor:")
     var valor = readLine()!!.toInt()
 
@@ -13,4 +17,9 @@ fun tempo( ano : Int) {
     print("Horas %d ".format(ano * 12 * 24))
     print("Minutos %d ".format(ano * 12 * 24 * 60))
     print("Segundos %d ".format(ano * 12 * 24 * 60 * 60))
+}
+
+fun chars( palavra: String) : Int {
+    return palavra.length
+
 }
