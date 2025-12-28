afa07f8f4a4834926c9ff4112530cd7624b66bc9
Fabio Leandro Lapuinka <lapuinkafabio@gmail.com>
2025-12-27 23:15:40 -0300

    day

--- src/main/kotlin/LeituraDados.kt
+++ src/main/kotlin/LeituraDados.kt
@@ -1,5 +1,6 @@
-fun main(){
+fun main() {
 
+    println("Temperatura %f F".format(CelsiusFahrenheit(10.0)))
     println("Milhas %f".format(milhas(8.0)))
     println("Dados: ")
     println(chars("Hello World!"))
@@ -12,7 +13,7 @@ fun main(){
 
 }
 
-fun tempo( ano : Int) {
+fun tempo(ano: Int) {
 
     print("Meses %d ".format(ano * 12))
     print("Horas %d ".format(ano * 12 * 24))
@@ -20,12 +21,21 @@ fun tempo( ano : Int) {
     print("Segundos %d ".format(ano * 12 * 24 * 60 * 60))
 }
 
-fun chars( palavra: String) : Int {
+fun chars(palavra: String): Int {
     return palavra.length
 
 }
 
-fun milhas( km: Double) : Double {
-    return km * 1.6
+fun milhas(milhas: Double): Double {
+    return milhas * 1.6
 
+}
+
+fun ex3(n: Double): Double {
+    return n * 3
+}
+
+fun CelsiusFahrenheit(celsius : Double): Double
+{
+    return  (celsius * 9 / 5 ) + 32
 }
