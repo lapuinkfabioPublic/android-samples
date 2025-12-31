8073e402572faa0eef216b1edb7cf07fe19d39b4
Fabio Leandro Lapuinka <lapuinkafabio@gmail.com>
2025-12-31 17:46:08 -0300

    day

--- src/main/kotlin/LeituraDados.kt
+++ src/main/kotlin/LeituraDados.kt
@@ -3,6 +3,8 @@ import java.util.Locale.getDefault
 
 fun main() {
 
+    var dia = 0
+    println(DiaSemana(dia))
 
     var palavra = leString()
     println(TrocaString(palavra ))
@@ -57,4 +59,22 @@ fun TrocaString(palavra : String) : String
     if(!palavra.contains("a"))
         return palavra.uppercase(getDefault()).toString()
     return palavra.lowercase().toString().replace("a","X", true)
+}
+fun DiaSemana(dia: Int): String {
+    if(dia == 0)
+        return "Domingo"
+    else if(dia == 1)
+        return "Segunda"
+    else if(dia == 2)
+        return "Terça"
+    else if(dia == 3)
+        return "Quarta"
+    else if(dia == 4)
+        return "Quinta"
+    else if(dia == 5)
+        return "Sexta"
+    else if(dia == 6)
+        return "Sábado"
+    else
+        return dia.toString()
 }
