9196908ba73410046e5342f2bdb3565976c331a6
Fabio Leandro Lapuinka <lapuinkafabio@gmail.com>
2026-01-06 13:33:34 -0300

    day

--- src/main/kotlin/exception.kt
+++ src/main/kotlin/exception.kt
@@ -3,5 +3,17 @@ fun main()
     val szPalavra  : String?
     szPalavra = null
 
-    println(szPalavra)
+    println(szPalavra?.length)
+   // println(szPalavra!!) //neste caso, pode lancar a exception pq eu garanto que vou tratar
+
+    var abc = readLine()
+    if (abc != null)
+    {
+        println("Valor não nulo")
+    }
+    else
+    {
+        println("valor nulo")
+    }
 }
+
